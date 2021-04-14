2923a1e34e340c682604fe2d377c0fbfed86936c
brunothomazsantos@gmail.com <brunothomazsantos@gmail.com>
2021-04-14 15:08:38 -0300

    setup: dotenv-express-mongoose-helmet-cors

---
+++ server/src/models/logentry.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const requiredNumber = {
+    type: Number,
+    required: true,
+};
+
+const logEntrySchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    comments: String,
+    image: String,
+    rating: {
+        type: Number,
+        min: [0,'The worst location ever'],
+        max: [10, "Your best location ever!"],
+        default : 0,
+    },
+    latitude: {
+        requiredNumber,
+        min:-90,
+        max: 90,
+    },
+    longitude: {
+        requiredNumber,
+        min:-180,
+        max: 180,
+    },
+    visitDate: {
+        required: true,
+        type: Date,
+        default: Date.now,
+    }        
+
+},
+{
+    timestamps: true,
+});
+
+
+
+module.exports = mongoose.model('LogEntry', logEntrySchema);
